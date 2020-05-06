acfd2119d02c2ba713b8720cb0af18999859b8a9
KatarzynaPalka <katarzynapalka928@gmail.com>
2020-05-06 23:11:41 +0200

    Początek aplikacji blog - paginacja wymaga korekty

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
 # Create your views here.
 
+
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # po 3 posty na każdej stronie
+    page = request.GET.get('page')  # parametr GET.page wskazującego na numer bieżącej strony
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # jeżeli zmienna page nie jest liczbą całkowitą to pobierana jest pierwsza strona wyników
+        posts = paginator.page(1)
+    except EmptyPage:
+        # jeżeli zmienna page ma wartość większą niż numer ostatniej strony wyników
+        # to pobierana jest ostatnia strona wyników
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
