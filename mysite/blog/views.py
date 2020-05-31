947ab42b4eb0454cbd76bad1b2436872704a6bfc
KatarzynaPalka <katarzynapalka928@gmail.com>
2020-05-31 19:48:23 +0200

    Aplikacja blog - tworzenie komentarzy do postów cz. 2

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 
@@ -39,11 +39,26 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published',
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # Lista aktywnych formularzy dla danego posta
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+            comment_form = CommentForm()
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'comment_form': comment_form})
 
 
 def test(request):
