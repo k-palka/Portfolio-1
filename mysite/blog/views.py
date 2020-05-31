ffd9b733f4cd7a7b7484cf827064053329adc5a0
KatarzynaPalka <katarzynapalka928@gmail.com>
2020-05-31 13:36:22 +0200

    Aplikacja blog - generowanie formularza w szablonie bez mail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -74,3 +74,4 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+
